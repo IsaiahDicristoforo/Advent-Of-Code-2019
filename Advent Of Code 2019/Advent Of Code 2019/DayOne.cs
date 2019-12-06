51091146fab4a767d4c7a11eab8cbd0557ed983f
Isaiah Dicristoforo <48969369+IsaiahDicristoforo@users.noreply.github.com>
2019-12-06 12:08:08 -0500

    Solved Day Two Part Two

---
+++ Advent Of Code 2019/Advent Of Code 2019/DayOne.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent_Of_Code_2019
+{
+    public class DayOne
+    {
+
+        public static double SolveDay2Part2()
+        {
+
+            double totalFuel = 0;
+
+            double[] masses = File.ReadAllLines("../../DayOneFiles/Mass.txt").Select(x => Convert.ToDouble(x)).ToArray();
+
+            foreach (var mass in masses)
+            {
+                double fuel = Math.Floor(((mass)) / 3.0) - 2;  //Initial Fuel
+                totalFuel += fuel;  //Add to total
+                while (fuel >= 0)
+                {
+
+                    fuel = Math.Floor(fuel / 3.0) - 2;
+
+                    if (fuel > 0)
+                    {
+                        totalFuel += fuel;
+
+                    }
+
+
+                }
+
+            }
+
+            return totalFuel;
+
+
+        }
+
+        public static double SolveDay1Part1()
+        {
+            //Sources:
+            //https://stackoverflow.com/questions/22086692/get-sum-of-the-value-from-list-using-linq
+
+            return File.ReadAllLines("../../DayOneFiles/Mass.txt").Sum(x => (Math.Floor(Convert.ToDouble(x) / 3.0)) - 2);
+        }
+    }
+}
--- Advent Of Code 2019/Advent Of Code 2019/Program.cs
+++ Advent Of Code 2019/Advent Of Code 2019/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +13,12 @@ namespace Advent_Of_Code_2019
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(Day01Problem01.SolveProblemOne());
+            Console.WriteLine(DayOne.SolveDay1Part1());
+            Console.WriteLine(DayOne.SolveDay2Part2());
         }
+
+      
+
+
     }
 }
